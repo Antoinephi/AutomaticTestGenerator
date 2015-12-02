575ed5daae8fdaaec7c82c9cdfb69bd65c7c1716
kevinonce <once@b10p13.fil.univ-lille1.fr>
2015-12-02 17:25:15 +0100

    architecture done :)

--- src/processors/TestSkeletonGeneratorProcessor.java
+++ src/processors/TestSkeletonGeneratorProcessor.java
@@ -1,16 +1,58 @@
 package processors;
 
+import java.lang.annotation.Annotation;
+import java.util.Set;
+
+import jdk.nashorn.internal.runtime.regexp.JoniRegExp.Factory;
 import spoon.processing.AbstractProcessor;
+import spoon.reflect.code.CtBlock;
+import spoon.reflect.code.CtCodeSnippetStatement;
+import spoon.reflect.declaration.CtAnnotation;
 import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.declaration.CtPackage;
+import spoon.reflect.declaration.ModifierKind;
+import spoon.reflect.factory.AnnotationFactory;
+import spoon.reflect.reference.CtTypeReference;
+import spoon.support.reflect.declaration.CtAnnotationImpl;
+import spoon.support.reflect.reference.CtTypeReferenceImpl;
 
 public class TestSkeletonGeneratorProcessor extends AbstractProcessor<CtClass<?>> {
 
-	public void process(CtClass<?> arg0) {
-		
+	public void process(CtClass<?> c) {
+		if(c.isTopLevel() && !c.hasModifier(ModifierKind.ABSTRACT)){
+			CtPackage p = 	getFactory().Core().createPackage();
+			p.setSimpleName("test");
+			CtClass<?> newClass = getFactory().Class().create(getFactory().Package().create(p, c.getPackage().getSimpleName()) ,c.getSimpleName()+"Test");
+			Set<CtMethod<?>> methodsList = c.getAllMethods();
+			for(CtMethod<?> m : methodsList){
+				if(m.hasModifier(ModifierKind.PUBLIC)){
+					CtMethod<Void> newMethod = newClass.getFactory().Core().createMethod();
+					newMethod.setType(getFactory().Type().VOID);
+					
+					newMethod.setSimpleName("test" + m.getSimpleName());
+					newMethod.setVisibility(m.getVisibility());
+					newMethod.setParameters(m.getParameters());
+					
+			        final CtCodeSnippetStatement statementInConstructor = getFactory().Code().createCodeSnippetStatement("junit.framework.Assert.fail()");
+			        final CtBlock ctBlockOfConstructor = getFactory().Code().createCtBlock(statementInConstructor);
+					
+			        newMethod.setBody(ctBlockOfConstructor);
+					CtAnnotation<?> annotation = new CtAnnotationImpl<>();					
+					annotation.setAnnotationType(getFactory().Annotation().createReference(org.junit.Test.class));
+					
+					newMethod.addAnnotation(annotation);
+					
+					newClass.addMethod(newMethod);
+					
+					
+				}
+			}
+		}
 	}
 
 	public void processingDone(){
-		
+		System.out.println("Done !");
 	}
 	
 }
