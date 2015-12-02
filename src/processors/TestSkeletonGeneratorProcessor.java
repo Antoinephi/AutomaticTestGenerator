0c98985d4d8e78e57d2eb8f9c6f2689c6024d271
Mandrivia <antoine.philippe62@gmail.com>
2015-12-02 14:40:22 +0100

    initial code skeletton commit

---
+++ src/processors/TestSkeletonGeneratorProcessor.java
@@ -0,0 +1,16 @@
+package processors;
+
+import spoon.processing.AbstractProcessor;
+import spoon.reflect.declaration.CtClass;
+
+public class TestSkeletonGeneratorProcessor extends AbstractProcessor<CtClass<?>> {
+
+	public void process(CtClass<?> arg0) {
+		
+	}
+
+	public void processingDone(){
+		
+	}
+	
+}
