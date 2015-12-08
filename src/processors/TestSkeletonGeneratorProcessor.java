f6777a0930f2e4020831f115b2d8ce608537381a
kevinonce <once@b10p13.fil.univ-lille1.fr>
2015-12-08 16:41:11 +0100

    génération du constructeur

--- src/processors/TestSkeletonGeneratorProcessor.java
+++ src/processors/TestSkeletonGeneratorProcessor.java
@@ -3,9 +3,11 @@ package processors;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
+import filter.IntegerMethodFilter;
 import spoon.processing.AbstractProcessor;
 import spoon.reflect.code.CtBlock;
 import spoon.reflect.code.CtCodeSnippetStatement;
@@ -14,6 +16,7 @@ import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.declaration.CtPackage;
 import spoon.reflect.declaration.ModifierKind;
+import spoon.reflect.visitor.Query;
 import spoon.support.reflect.declaration.CtAnnotationImpl;
 
 public class TestSkeletonGeneratorProcessor extends AbstractProcessor<CtClass<?>> {
@@ -30,9 +33,9 @@ public class TestSkeletonGeneratorProcessor extends AbstractProcessor<CtClass<?>
 			CtPackage p = 	getFactory().Core().createPackage();
 			p.setSimpleName("test");
 			CtClass<?> newClass = getFactory().Class().create(getFactory().Package().create(p, c.getPackage().getSimpleName()) ,c.getSimpleName()+"Test");
-			Set<CtMethod<?>> methodsList = c.getAllMethods();
+			List<CtMethod<?>> methodsList =  Query.getElements( c, new IntegerMethodFilter());
 			for(CtMethod<?> m : methodsList){
-				if(m.hasModifier(ModifierKind.PUBLIC)){
+				if(m.hasModifier(ModifierKind.PUBLIC) && m.getParent().equals(c)){
 					CtMethod<Void> newMethod = newClass.getFactory().Core().createMethod();
 					newMethod.setType(getFactory().Type().VOID);
 					
@@ -40,25 +43,36 @@ public class TestSkeletonGeneratorProcessor extends AbstractProcessor<CtClass<?>
 					newMethod.setVisibility(m.getVisibility());
 					newMethod.setParameters(m.getParameters());
 					
-			        final CtCodeSnippetStatement statementInConstructor = getFactory().Code().createCodeSnippetStatement("junit.framework.Assert.fail()");
+
+					
+					CtAnnotation<?> annotation = new CtAnnotationImpl<>();					
+					annotation.setAnnotationType(getFactory().Annotation().createReference(org.junit.Test.class));
+					newMethod.addAnnotation(annotation);
+					
+					List<CtCodeSnippetStatement> listeInstruction = generateMethodResult(classe, m);
+			        final CtCodeSnippetStatement statementInConstructor = listeInstruction.get(0);
 			        final CtBlock ctBlockOfConstructor = getFactory().Code().createCtBlock(statementInConstructor);
 					
 			        newMethod.setBody(ctBlockOfConstructor);
-					CtAnnotation<?> annotation = new CtAnnotationImpl<>();					
-					annotation.setAnnotationType(getFactory().Annotation().createReference(org.junit.Test.class));
-					
-					newMethod.addAnnotation(annotation);
+			        
+					for(int i=1;i<listeInstruction.size();i++){
+				        newMethod.getBody().addStatement(listeInstruction.get(i));
+					}
 					
 					newClass.addMethod(newMethod);
-					
-					generateMethodResult(classe, m);
+
+
 				}
 			}
 		}
 	}
 
-	private void generateMethodResult(Class classe, CtMethod<?> m) {
+	private List<CtCodeSnippetStatement> generateMethodResult(Class classe, CtMethod<?> m) {
+		List<CtCodeSnippetStatement> listeInstruction = new ArrayList<>();
+		listeInstruction.add(getFactory().Code().createCodeSnippetStatement("junit.framework.Assert.fail()"));
+
 		if(classe != null){
+			Integer valeur = 1;
 			 try {							 
 				 Class tab[] = new Class[m.getParameters().size()];
 				 for(int i=0;i<tab.length;i++){
@@ -66,7 +80,8 @@ public class TestSkeletonGeneratorProcessor extends AbstractProcessor<CtClass<?>
 				 }
 				Method methode= classe.getMethod(m.getSimpleName(), tab);
 				if(tab.length == 0 && hasConstructorWithoutParameter(classe)){
-					System.out.println(methode.getName());
+					
+					listeInstruction.add(getFactory().Code().createCodeSnippetStatement(classe.getCanonicalName()+" "+classe.getSimpleName()+(valeur++)+" = new "+classe.getCanonicalName()+"()"));
 					Object retour = methode.invoke(classe.newInstance(), (Object[])null);
 				}
 			} catch (NoSuchMethodException e) {
@@ -90,6 +105,8 @@ public class TestSkeletonGeneratorProcessor extends AbstractProcessor<CtClass<?>
 			}
 			
 		}
+		
+		return listeInstruction;
 	}
 
 	private boolean hasConstructorWithoutParameter(Class classe) {
