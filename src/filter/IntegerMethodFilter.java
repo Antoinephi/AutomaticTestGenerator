9cb0b444e91a81ee195fa10ce2352b03d8e3511c
Mandrivia <antoine.philippe62@gmail.com>
2015-12-15 18:48:22 +0100

    fix, ordre verif type du filtre

--- src/filter/IntegerMethodFilter.java
+++ src/filter/IntegerMethodFilter.java
@@ -1,15 +1,24 @@
 package filter;
 
 import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.declaration.CtParameter;
 import spoon.reflect.visitor.Filter;
 
 public class IntegerMethodFilter implements Filter<CtMethod<?>> {
 
 	@Override
 	public boolean matches(CtMethod<?> method) {
+		
+		for(CtParameter<?> parametre : method.getParameters()){
+			if(!parametre.getReference().getType().isPrimitive()){
+				return false;
+			}
+		}
+		
 		if(method.getType().getActualClass().equals(Integer.class) || method.getType().getActualClass().equals(int.class)){
 			return true;
 		}
+		
 		
 		return false;
 	}
